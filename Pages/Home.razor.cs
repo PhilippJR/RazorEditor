00861ad576218fd34b56583e19e7d4aa724bb081
PhilippJR <dr.philipp.rollmann@walterroller.de>
2026-04-30 08:40:25 +0200

    In Home.razor, add code an description that show that CTRL+K,D does not work properly.

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorEditor.Pages
+
+{
+    public partial class Home
+    {
+        readonly bool _bool1 = true;
+        readonly bool _bool2 = true;
+        readonly bool _bool3 = true;
+
+        public string MyString { get; set; } = "";
+    }
+}
