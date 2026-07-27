26c15e51313800706b9b3509915ca26154d04f78
PhilippJR <dr.philipp.rollmann@walterroller.de>
2026-07-27 13:20:35 +0200

    Delete the code that is no longer needed, since the unexpected behavior is not considered an error.

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -4,23 +4,5 @@
     public partial class Home
     {
         readonly bool _bool1 = true;
-        readonly bool _bool2 = true;
-        readonly bool _bool3 = true;
-
-        public string MyString { get; set; } = "";
-
-
-        // VS 18.5: I expected CTRL+K,D to align the undescores.
-        // VS 18.6: I expected CTRL+K,D to align the undescores.
-        // VS 18.7: I expected CTRL+K,D to align the undescores.
-        private void MyMethod()
-        {
-            if (_bool1 ||
-_bool2 ||
-_bool3)
-            {
-
-}
-        }
     }
 }
